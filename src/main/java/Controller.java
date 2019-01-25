b13d099f6bc16b1d5074c393644662e57c3314ae
Тимофеев Алексей <ATimofeev@lanit.ru>
2019-01-25 18:57:28 +0300

    chess

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,5 +1,5 @@
-
 import java.util.Scanner;
+import org.apache.commons.math3.analysis.function.*;
 
 public class Controller {
 	
@@ -9,7 +9,9 @@ public class Controller {
 	public static void main(String[] args) {
 		Chessboard myChessboard = new Chessboard();
 
-
+		Cos cos = new Cos();		
+		System.out.println(cos.value(0));
+		
 		while (myChessboard.isGameRunning()) {
 
 			myChessboard.printBoard();
